f6b1a63b75d6f816adb3c99228d1e28352808ad2
kabilan-s <kabilanselvakumar313@gmail.com>
2024-05-25 13:42:41 +0530

    Update page.jsx

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -2,7 +2,9 @@ import EditTopicForm from "@/components/EditTopicForm";
 
 const getTopicById  = async(id) =>{
     try{
-        const res = await fetch(`http://localhost:3000/api/topics/${id}`);
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`,{
+            cache:'no-store',
+        });
         if(!res.ok){
             throw new Error("Failed to fetch topic")
         }
