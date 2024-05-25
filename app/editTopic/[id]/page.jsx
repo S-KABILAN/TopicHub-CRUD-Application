06f0eb3510dfa6e62047b13947f3439953b72968
kabilan-s <kabilanselvakumar313@gmail.com>
2024-05-25 13:40:02 +0530

    no store removed

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -2,9 +2,7 @@ import EditTopicForm from "@/components/EditTopicForm";
 
 const getTopicById  = async(id) =>{
     try{
-        const res = await fetch(`http://localhost:3000/api/topics/${id}`,{
-            cache:'no-store',
-        });
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`);
         if(!res.ok){
             throw new Error("Failed to fetch topic")
         }
